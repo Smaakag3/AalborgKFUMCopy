0e7e766571c012bb6ad363cd466de7e6d887da7e
DimiGreens <10632176@ucn.dk>
2025-06-03 14:55:58 +0200

    Har fået requesten til at virke

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,21 @@
-const domain = "https://aalborgkfumfodbold.r-dalsgaard.dk/";
-const endPoint = "wp-json/wp/v2/posts";
+const domain = "https://aalborgkfumfodbold.r-dalsgaard.dk/"
+const endPoint = "wp-json/wp/v2/posts?acf_format=standard"
+const mainEl = document.querySelector("main")
 
 fetch(domain + endPoint)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log(data);
+    renderData(data);
+})
 .catch(err => console.log(err))
 
-// fetch(domain + endPoint + "?taxonomy=argang&tag_ID=7")
-// .then(res => res.json())
-// .then(data => console.log(data))
-// .catch(err => console.log(err))
+function renderData(data){
+    data.forEach(hold => {
+        mainEl.innerHTML += `
+        <h1>${hold.acf.hold_navn}</h1>
+        <h2>${hold.acf.aargang}</h2>
+        <p>${hold.acf.beskrivelse}</p>
+        `
+    })
+}
