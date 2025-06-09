096f7a2943ea88caf2a97e126d2413d307352044
Emilia-v-p19 <emilia@pretsch.dk>
2025-06-09 16:00:14 +0200

    kommentar til kode

--- assets/js/maalsaetninger.js
+++ assets/js/maalsaetninger.js
@@ -1,11 +1,13 @@
 function aabenOrdenlighed() {
   document.getElementById("ordenlighedinfo").style.display = "block";
 }
+//naar man cliker paa korted ordenlighed saa aendre ordenlighedinfo style til block saa man kan se den//
 
 function lukOrdenlighed() {
   document.getElementById("ordenlighedinfo").style.display = "none";
 }
-
+//naar man cliker paa luk knappen saa bliver ordenlighedinfo til none igen og kan ikke ses
+ 
 function aabenFaellesskab() {
   document.getElementById("faellesskabinfo").style.display = "block";
 }
