12be21031309057d56b09ef2aa4580476dc28d63
Emilia-v-p19 <emilia@pretsch.dk>
2025-06-09 11:05:27 +0200

    opsaestning af cards i css + styling

---
+++ assets/js/maalsaetninger.js
@@ -0,0 +1,23 @@
+function aabenOrdenlighed() {
+  document.getElementById("ordenlighedinfo").style.display = "block";
+}
+
+function lukOrdenlighed() {
+  document.getElementById("ordenlighedinfo").style.display = "none";
+}
+
+function aabenFaellesskab() {
+  document.getElementById("faellesskabinfo").style.display = "block";
+}
+
+function lukFaellesskab() {
+  document.getElementById("faellesskabinfo").style.display = "none";
+}
+
+function aabenUdvikling() {
+  document.getElementById("udviklinginfo").style.display = "block";
+}
+
+function lukUdvikling() {
+  document.getElementById("udviklinginfo").style.display = "none";
+}
